535e7b922bffc7b1e24e615efa082bfda9a6f37f
biqu@micron <biqu@micron>
2024-08-24 19:55:38 +0200

    Autocommit from 2024-08-24 19:55:38
    Klipper on commit: 81de9a8
    
    Moonraker on commit: f735c04
    
    Mainsail version: v2.12.0

--- tools/clean_tempfiles.sh
+++ tools/clean_tempfiles.sh
@@ -1,5 +1,5 @@
-find ~/printer_data/gcodes/ -type f -mtime +20 -delete
-find ~/printer_data/config/gcodes-*.zip -type f -delete
-find ~/printer_data/config/config-*.zip -type f -delete
-find ~/printer_data/config/printer-*.cfg -type f -mtime +10 -delete
-
+#!/bin/bash
+find ~/printer_data/gcodes/ -type f -mtime +20 -delete -print
+find ~/printer_data/config/ -name gcodes-*.zip -type f -delete -print
+find ~/printer_data/config/ -name config-*.zip -type f -delete -print
+find ~/printer_data/config/ -name printer-*.cfg -type f -mtime +10  -print
