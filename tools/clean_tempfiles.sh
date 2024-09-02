299e4b3ff5ad140e0e22adfae183ff95cfbb7591
biqu@micron <biqu@micron>
2024-09-02 09:17:33 +0200

    Autocommit from 2024-09-02 09:17:33
    Klipper version: v0.12.0-286-g81de9a861
    
    Moonraker version: v0.9.2-6-gf735c04
    
    Mainsail version: v2.12.0

--- tools/clean_tempfiles.sh
+++ tools/clean_tempfiles.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-find ~/printer_data/gcodes/ -type f -mtime +20 -delete -print
-find ~/printer_data/config/ -name gcodes-*.zip -type f -delete -print
-find ~/printer_data/config/ -name config-*.zip -type f -delete -print
-find ~/printer_data/config/ -name printer-*.cfg -type f -mtime +10  -print
+# find ~/printer_data/gcodes/ -type f -mtime +20 -delete -print
+# find ~/printer_data/config/ -name gcodes-*.zip -type f -delete -print
+# find ~/printer_data/config/ -name config-*.zip -type f -delete -print
+# find ~/printer_data/config/ -name printer-*.cfg -type f -mtime +10  -delete -print
+# find /tmp/scanner-calibrate-*.csv -type f -mtime +1  -delete -print
