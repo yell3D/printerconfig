f203bc41fa0688fb274e26e85327203158390376
pi@micron <pi@micron.lan>
2025-01-03 19:33:07 +0100

    Autocommit from 2025-01-03 19:33:07
    Klipper version: v0.12.0-574-g4089c357
    
    Moonraker version: v0.9.3-4-ga4604e3
    
    Mainsail version: v2.13.2

--- tools/config_pull.sh
+++ tools/config_pull.sh
@@ -1,18 +1,10 @@
 #!/bin/bash
 
 
-### Changes
-# Feb 12, 2024
-#   added auto banch name
-# Jan 29, 2024 
-#   created
-###
-
-
 config_folder=~/printer_data/config/printerconfig
+cd $config_folder
+
 branch=$(git branch -vv | grep -Po  "^[\s\*]*\K[^\s]*(?=.*$(git branch -rl '*/HEAD' | grep -o '[^ ]\+$'))")
 
-
-cd $config_folder
 git pull origin $branch --no-rebase
 
