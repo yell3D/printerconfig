7181e6970036fde19059044035f3e0f2310741e6
dietpi@vtcat <pi@>
2025-03-01 00:25:19 +0100

    Autocommit from 2025-03-01 00:25:19
    Klipper version: v0.12.0-638-gef9b5391
    
    Moonraker version: v0.9.3-64-g4889a2d
    
    Mainsail version: v2.13.2

--- tools/clean_tempfiles.sh
+++ tools/clean_tempfiles.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-find ~/printer_data/logs/ -type f -mtime +1 -delete -print
+find ~/printer_data/logs/ -type f -mtime +5 -delete -print
 find ~/printer_data/gcodes/ -type f -mtime +20 -delete -print
 
-find ~/printer_data/config/ -name gcodes-*.zip -type f -delete -print
+find ~/printer_data/config/ -name *.zip -type f -delete -print
 find ~/printer_data/config/ -name config-*.zip -type f -delete -print
 find ~/printer_data/config/ -name printer-*.cfg -type f -mtime +10  -delete -print
 
